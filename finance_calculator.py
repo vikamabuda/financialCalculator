bc77442aa06b391c35724d126f5cd2e7852efd59
vikamabuda <vikamabuda900@gmail.com>
2023-11-08 09:26:43 +0200

    changes

--- finance_calculator.py
+++ finance_calculator.py
@@ -19,15 +19,15 @@ if user_choice in user_investment:
         else:
             interest_rate = int(interest_rate_input)
 
-        period_of_investment = int(input("How long are you planning to invest? (Years): "))
+        period_of_investment = int(input("How long are you planning to invest? (Months): "))  # Change to months
         interest = input("Do you want 'simple' or 'compound' interest: ").lower()
 
         if interest == "simple":
-            future_amount = round(user_deposit_amount * (1 + interest_rate * period_of_investment / 100), 2)
-            print("The amount you will have is : R%.2f" % future_amount)
+            future_amount = round(user_deposit_amount * (1 + interest_rate * period_of_investment / 1200), 2)
+            print("The amount you will have is: R%.2f" % future_amount)
         elif interest == "compound":
-            future_amount = round(user_deposit_amount * math.pow(1 + interest_rate / 100, period_of_investment), 2)
-            print("The total amount you will have is : R%.2f" % future_amount)
+            future_amount = round(user_deposit_amount * math.pow(1 + interest_rate / 1200, period_of_investment), 2)
+            print("The total amount you will have is: R%.2f" % future_amount)
         else:
             print("Invalid interest type. Please choose 'simple' or 'compound'.")
     except ValueError:
@@ -36,23 +36,22 @@ if user_choice in user_investment:
 elif user_choice in user_bond:
     try:
         present_value = float(input("What is the present value of the house?: "))
-        interest_rate_input = input("What is the annual interest rate? (e.g., 7.7 or 7, not 7%): ")
+        annual_interest_rate_input = input("What is the annual interest rate? (e.g., 7.7 or 7, not 7%): ")
 
-        if "." in interest_rate_input:
-            annual_interest_rate = float(interest_rate_input)
+        if "." in annual_interest_rate_input:
+            annual_interest_rate = float(annual_interest_rate_input)
         else:
-            annual_interest_rate = int(interest_rate_input)
+            annual_interest_rate = int(annual_interest_rate_input)
 
         period_of_return = int(input("How long do you plan to repay the bond? (Months): "))
 
-        monthly_interest_rate = annual_interest_rate / 12
-        monthly_payment = round((monthly_interest_rate * present_value) / (1 - math.pow(1 + monthly_interest_rate, - period_of_return)), 2)
+        monthly_interest_rate = annual_interest_rate / 12 / 100
+        monthly_payment = round((monthly_interest_rate * present_value) / (1 - math.pow(1 + monthly_interest_rate, -period_of_return)), 2)
 
-
-        print("You will need to pay {monthly_payment : R%.2f" % monthly_payment)
+        print("You will need to pay monthly: R%.2f" % monthly_payment)
 
     except ValueError:
-         print("Invalid input. Please enter valid numerical values.")
+        print("Invalid input. Please enter valid numerical values.")
 
 else:
     print("You have not entered a valid option. Please choose 'investment' or 'bond'.")
